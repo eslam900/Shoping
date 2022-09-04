c345777c6b8d1dbb261dd4ba592c7186f40c18b7
eslam900 <eslam_thelegend2011@yahoo.com>
2022-09-04 22:44:15 +0200

    nnn

--- src/component/NavBestNew.js
+++ src/component/NavBestNew.js
@@ -10,7 +10,7 @@ useEffect(()=>{
 },[])
 
 const HandelClick = (type)=>{
-  if(type !== "All"){
+  if(type){
     Setitem(data.filter((el)=>el.status === type))
   }else{
     Setitem(data.filter((el)=>el.status !== undefined))
@@ -18,10 +18,10 @@ const HandelClick = (type)=>{
 }
   return (
     <section className='container mt-5'>
-    <ul className='navbar mb-0 justify-content-center gap-5 p-0'>
-    <li className='nav_Best fs-3 fw-bold text-secondary opacity-50 list-unstyled Activ' onClick={()=>HandelClick("All")} role='button'>Best Sellers</li>
-    <li className='nav_Best fs-3 fw-bold text-secondary opacity-50 list-unstyled' onClick={()=>HandelClick("new")} role='button'>New Arrivals</li>
-    <li className='nav_Best fs-3 fw-bold text-secondary opacity-50 list-unstyled' onClick={()=>HandelClick("sale")} role='button'>Hot Sales</li>
+    <ul className='navbar mb-0 justify-content-center gap-4 p-0'>
+    <li className='nav_Best fs-3 fw-bold text-secondary opacity-50 list-unstyled Activ' onClick={()=>HandelClick()} role='button'>Best Sellers</li>
+    <li className='nav_Best fs-3 fs-sm-3 fw-bold text-secondary opacity-50 list-unstyled' onClick={()=>HandelClick("new")} role='button'>New Arrivals</li>
+    <li className='nav_Best fs-3 fs-sm-3 fw-bold text-secondary opacity-50 list-unstyled' onClick={()=>HandelClick("sale")} role='button'>Hot Sales</li>
     </ul>
     <div className=" container-sm d-flex flex-wrap align-items-center mt-2 justify-content-center">{Item.map((el,ind)=> <CardShop key={ind} item={el} id={el.id}/> )}</div>
     </section>
